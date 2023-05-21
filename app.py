31b23cc94757a8f209c71d0c880c8fc699808afc
oriaaburime <oriaaburime@gmail.com>
2023-05-21 21:37:37 +0200

    bugs removed please review

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 # This is a test for banking software application
 # Developers will works in groups to develop the software
+print ('simon')
+print ('ivo')
+# echo simon
+print ('chidi')
+print ('grace')
+print ('glory')
+print ('LSS')
+bugs removed
